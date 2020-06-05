6e68f245835c3b5924e357616bc170060456c036
Kapil M <kapil@eventtan.com>
2020-06-05 17:42:46 +0530

    Xamarin Map Demo

---
+++ MapDemo.Android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android;
+
+namespace MapDemo.Droid
+{
+    [Activity(Label = "MapDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            if (requestCode == RequestLocationId) {
+                if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted)) {
+                    Console.WriteLine("Location permissions granted.");
+                } else {
+                    Console.WriteLine("Location permissions denied.");
+                }
+            } else {
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if ((int)Build.VERSION.SdkInt >= 23) {
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted) {
+                    RequestPermissions(LocationPermissions, RequestLocationId);
+                } else {
+                    Console.WriteLine("Location permissions already granted.");
+                }
+            }
+        }
+    }
+}
